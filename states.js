0fd8d171f58df3341159091d9ccccde889421f84
Samuel <122253166+RAESRSE7EN@users.noreply.github.com>
2024-03-16 09:22:16 +0400

    Update states.js

--- states.js
+++ states.js
@@ -1,12 +1,16 @@
-
-
-
-function showTooltip(text) {
-  const stateInfo = document.getElementById('state-info');
+  function showTooltip(text) {
+    const stateInfo = document.getElementById('state-info');
     stateInfo.textContent = text;
-  stateInfo.style.display = 'block';
-} 
-function hideTooltip() {
-  const stateInfo = document.getElementById('state-info');
-  stateInfo.style.display = 'none';
-}
+    stateInfo.style.display = 'block';
+    
+  const x = event.clientX + 10;
+  const y = event.clientY + 10;
+
+  stateInfo.style.left = `${x}px`;
+  stateInfo.style.top = `${y}px`;
+  }
+  
+  function hideTooltip() {
+    const stateInfo = document.getElementById('state-info');
+    stateInfo.style.display = 'none';
+  }
